6cbb5f8e06d363d9a972c33949063f6f95790155
Roman Smirnov <roman@railsdog.com>
2011-04-27 21:30:31 +0400

    Added creating Openbravo product based on Spree product

--- app/models/openbravo/product.rb
+++ app/models/openbravo/product.rb
@@ -3,32 +3,35 @@ module Openbravo
 # 
 #  product  = Openbravo::Product.find("8A64B71A2B0B2946012B0BC436170107").attributes["Product"]
 #  products = Openbravo::Product.all(:params => {:maxResult => 3})
-#  product  = Openbravo::Product.new(:name => "RoR-Mug", :product_category => "Ruby on Rails", :searchKey => "ROR-101")
+#  Openbravo::Product.create(Product.first)
 #
   class Product < Base
     self.element_name = "Product"
     self.collection_name = "Product"
+    attr_accessor :product_category_id
     validates :name, :searchKey, :product_category_id, :presence => true
 
-    def product_category_id
-      return nil unless self.product_category
-      cat = Openbravo::ProductCategory.last(:params => {:where => "searchKey='#{self.product_category}'"})
-      return cat.id if cat.present?
-      ProductCategory.create(:name => self.product_category)
-      Openbravo::ProductCategory.last(:params => {:where => "searchKey='#{self.product_category}'"}).id
+    def self.create(product)
+      record = self.new(:name => product.name, :searchKey => "SP/#{product.permalink}")
+      product_category = product.taxons.first.try(:name) || "Spree"
+      cat = Openbravo::ProductCategory.last(:params => {:where => "searchKey='#{product_category}'"})
+      if cat.blank?
+        ProductCategory.create(:name => product_category)
+        cat = Openbravo::ProductCategory.last(:params => {:where => "searchKey='#{product_category}'"})
+      end
+      record.product_category_id = cat.id
+      res = record.save
+      Openbravo::Price.create(:product_search_key => "SP/#{product.permalink}", :standardPrice => product.cost_price || product.price, :listPrice => product.price)
+      res
     end
 
     def to_xml(options={})
-      product_category_id = self.product_category_id
-      self.attributes.delete(:product_category)
-      
       super(options) do |xml|
-        xml.productCategory nil, 'id' => product_category_id
+        xml.productCategory nil, 'id' => self.product_category_id
         xml.organization    nil, 'id' => Spree::Openbravo::Config[:organization_id]
         xml.taxCategory     nil, 'id' => Spree::Openbravo::Config[:tax_category_id]
         xml.uOM             nil, 'id' => '100' # Unit
         xml.productType "I" # Item
-        # xml.searchKey "ROR-101" should be unique for each product
       end
     end
   end
