9ff644aaa446685a283957b94ada4290d6c23fc3
Roman Smirnov <roman@railsdog.com>
2011-04-30 01:11:59 +0400

    Improved admin UI for openbravo integration settings

--- lib/spree_openbravo_hooks.rb
+++ lib/spree_openbravo_hooks.rb
@@ -1,3 +1,5 @@
 class SpreeOpenbravoHooks < Spree::ThemeSupport::HookListener
-  # custom hooks go here
+  insert_after :admin_configurations_menu do
+    "<%= configurations_menu_item(I18n.t('openbravo.settings'), admin_openbravo_settings_path, I18n.t('openbravo.manage_settings')) %>"
+  end
 end
