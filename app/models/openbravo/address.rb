2efd25f120e5e65168bb9c63ceaaf650dc9baec4
Roman Smirnov <roman@railsdog.com>
2011-04-30 21:12:23 +0400

    Replace ' in address lines to avoid error at Openbravo end

--- app/models/openbravo/address.rb
+++ app/models/openbravo/address.rb
@@ -6,9 +6,11 @@ module Openbravo
     attr_accessor :country_id, :region_id
     
     def self.create(address)
+      address_line1 = address.address1.gsub("'", "`")
+      address_line2 = address.address2.gsub("'", "`")
       attributes = {
-          :addressLine1 => address.address1, 
-          :addressLine2 => address.address2,
+          :addressLine1 => address_line1, 
+          :addressLine2 => address_line2,
           :cityName     => address.city,
           :postalCode   => address.zipcode,
       }
@@ -18,7 +20,7 @@ module Openbravo
       region = Openbravo::Region.last(:params => {:where => "name='#{address.state.abbr}'"})
       record.region_id = region.id
       record.save
-      Openbravo::Address.last(:params => {:where => "addressLine1='#{address.address1}'"})
+      Openbravo::Address.last(:params => {:where => "addressLine1='#{address_line1}'"})
     end
     
     def to_xml(options={})
